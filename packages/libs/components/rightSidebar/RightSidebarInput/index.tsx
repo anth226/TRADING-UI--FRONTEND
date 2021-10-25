b34612d577b786f0b3486ae50116123e7dc0818c
IntegralTeam <repo@sfxdx.ru>
2021-10-25 08:34:26 +0300

    update RightSidebarInput component

--- packages/libs/components/rightSidebar/RightSidebarInput/index.tsx
+++ packages/libs/components/rightSidebar/RightSidebarInput/index.tsx
@@ -1,8 +1,10 @@
 import React, { ChangeEventHandler, FC } from 'react';
+import cx from 'classnames';
 import styles from './styles.module.scss';
 import { FontIcon, FontIconName } from '../../inputs/FontIcon';
 
 interface Props {
+  className?: string
   label: string
   value?: string
   symbol: string
@@ -22,8 +24,9 @@ const RightSidebarInput: FC<Props> = ({
   onSecondBtnClick,
   firstBtnIcon,
   secondBtnIcon,
+  className,
 }) => (
-  <div className={styles.wrap}>
+  <div className={cx(styles.wrap, className)}>
     <div className={styles.content}>
       <p className={styles.label}>{label}</p>
       <div className={styles.input_wrap}>
