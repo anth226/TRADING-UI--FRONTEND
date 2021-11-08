4c18e393824fe87e3ea963b73321b7a971d93cfb
IntegralTeam <repo@sfxdx.ru>
2021-11-08 14:25:22 +0300

    MainChart TimeMark

--- packages/client/src/pages/Trading/index.tsx
+++ packages/client/src/pages/Trading/index.tsx
@@ -1,8 +1,19 @@
 import React, { FC } from 'react';
+import { AutoSizer } from 'react-virtualized';
 import { MainLayout } from '../../layouts/MainLayout';
+import { MainChart } from '../../components/charts/MainChart';
+import styles from './styles.module.scss';
 
 const Trading: FC = () => (
-  <MainLayout />
+  <MainLayout>
+    <div className={styles.chart_wrap}>
+      <AutoSizer>
+        {({ width, height }) => (
+          <MainChart width={width} height={height} />
+        )}
+      </AutoSizer>
+    </div>
+  </MainLayout>
 );
 
 export { Trading };
