0d6d70854bbfab31a8a4c61408e95c55eb877cfe
kudindmitriy <kudin.dima@gmail.com>
2022-06-27 10:44:10 +0300

    Add saga setting

--- packages/client/src/store/rootSaga.ts
+++ packages/client/src/store/rootSaga.ts
@@ -1,8 +1,13 @@
-import { fork } from 'redux-saga/effects';
+/* eslint-disable */
+//@ts-nocheck
+import { fork, all } from 'redux-saga/effects';
 import authSaga from './auth/sagas';
 import postsSaga from './posts/sagas';
 
 export default function* rootSaga() {
-  yield fork(authSaga);
-  yield fork(postsSaga);
+  yield all ([
+    fork(authSaga),
+    fork(postsSaga),
+  ])
+
 }
