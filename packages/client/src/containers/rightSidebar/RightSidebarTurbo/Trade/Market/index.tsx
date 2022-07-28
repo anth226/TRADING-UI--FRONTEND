79ec187e6964757f97eabecc2f0e9f185b7a1030
kudindmitriy <kudin.dima@gmail.com>
2022-07-28 16:05:21 +0300

    Create collapse(turbo)

--- packages/client/src/containers/rightSidebar/RightSidebarTurbo/Trade/Market/index.tsx
+++ packages/client/src/containers/rightSidebar/RightSidebarTurbo/Trade/Market/index.tsx
@@ -1,16 +1,22 @@
 /* eslint-disable */
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './styles.module.scss';
 import { Checkbox } from '@option-blitz/libs/components/inputs/Checkbox';
 import { RightSidebarInput } from '@option-blitz/libs/components/rightSidebar/RightSidebarInput';
 import cx from 'classnames';
-import { FontIconName } from '@option-blitz/libs/components/inputs/FontIcon';
-import copy from '../../../../Modals/ModalIcons/copy.svg';
+import {  FontIconName } from '@option-blitz/libs/components/inputs/FontIcon';
 import { Letter, letterIcons } from '../../../../../constants/letters/letters';
 import Button from '@option-blitz/libs/components/inputs/Button';
+import { Collapse } from '@option-blitz/libs/components/common/Collapse';
+import { useCollapse } from '../../../../../hooks/useCollapse';
 
 const Market = () => {
   const [active, setActive] = useState(true)
+  const wrapRef = useRef<HTMLDivElement>(null);
+  const {
+    isActive,
+    onChange,
+  } = useCollapse();
   return (
     <div>
       <div className={styles.checkbox_wrap}>
@@ -50,55 +56,68 @@ const Market = () => {
           secondIconClassName={styles.down_icon}
         />
       </div>
-      <div className={styles.summary_container}>
-        <div className={styles.summary}>SUMMMARY:</div>
-        <div className={styles.contract}>Contact info <img src={copy} alt='copy' className={styles.img_copy}/></div>
-      </div>
 
-      <div>
-        <div className={styles.title_wrap}>
-          <div className={styles.flex}>
-            <img src={letterIcons[Letter.L]} alt="letter" />
-            <p className={styles.title}>BTCUSD</p>
+      <div ref={wrapRef} className={styles.wrap}>
+        <Collapse
+          className={styles.collapse}
+          title={(
+            <div className={styles.title_wrap}>
+              <p className={styles.summary}>SUMMARY:</p>
+            </div>
+          )}
+          isActive={isActive}
+          onClick={onChange}
+        >
+          <div className={styles.chart_wrap}>
+            <div className={styles.summary_container}>
+            </div>
+
+            <div>
+              <div className={styles.title_wrap}>
+                <div className={styles.flex}>
+                  <img src={letterIcons[Letter.L]} alt="letter" />
+                  <p className={styles.title}>BTCUSD</p>
+                </div>
+                <div className={styles.time}>100x</div>
+              </div>
+
+              <div className={styles.item} >
+                <p className={styles.item_label}>Strike price</p>
+                <p className={styles.item_value}>$60,000.00</p>
+              </div>
+              <div className={styles.item}>
+                <p className={styles.item_label}>Knock-out (barrier) price</p>
+                <p className={styles.item_value}>$54,000.00</p>
+              </div>
+              <div className={styles.item}>
+                <p className={styles.item_label}>Take profit price</p>
+                <p className={styles.item_value}>$70,000.00</p>
+              </div>
+              <div className={styles.item} >
+                <p className={styles.item_label}>Current funding rate</p>
+                <p className={styles.item_value}>-0.015%</p>
+              </div>
+              <div className={styles.item} >
+                <p className={styles.item_label}>Quantity</p>
+                <p className={styles.item_value}>0.01 BTC</p>
+              </div>
+              <div className={styles.item} >
+                <p className={styles.item_label}>Transaction fee</p>
+                <p className={styles.item_value}>60 USDT | 30 BLX</p>
+              </div>
+              <div className={styles.item} >
+                <p className={styles.item_label}>Turbo price</p>
+                <p className={styles.item_value}>6000 USDC</p>
+              </div>
+
+            </div>
+
+            <Button  className={styles.button_trade}>
+              PLACE TRADE: 6000 USDC
+            </Button>
           </div>
-          <div className={styles.time}>100x</div>
-        </div>
-
-        <div className={styles.item} >
-          <p className={styles.item_label}>Strike price</p>
-          <p className={styles.item_value}>$60,000.00</p>
-        </div>
-        <div className={styles.item}>
-          <p className={styles.item_label}>Knock-out (barrier) price</p>
-          <p className={styles.item_value}>$54,000.00</p>
-        </div>
-        <div className={styles.item}>
-          <p className={styles.item_label}>Take profit price</p>
-          <p className={styles.item_value}>$70,000.00</p>
-        </div>
-        <div className={styles.item} >
-          <p className={styles.item_label}>Current funding rate</p>
-          <p className={styles.item_value}>-0.015%</p>
-        </div>
-        <div className={styles.item} >
-          <p className={styles.item_label}>Quantity</p>
-          <p className={styles.item_value}>0.01 BTC</p>
-        </div>
-        <div className={styles.item} >
-          <p className={styles.item_label}>Transaction fee</p>
-          <p className={styles.item_value}>60 USDT | 30 BLX</p>
-        </div>
-        <div className={styles.item} >
-          <p className={styles.item_label}>Turbo price</p>
-          <p className={styles.item_value}>6000 USDC</p>
-        </div>
-
+        </Collapse>
       </div>
-
-
-      <Button  className={styles.button_trade}>
-        PLACE TRADE: 6000 USDC
-      </Button>
 
     </div>
   );
